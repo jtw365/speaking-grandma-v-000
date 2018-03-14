4139fff5e23d64bc5413688864fe68e40007b95d
Jonathan Williams <jtw365@live.missouristate.edu>
2018-03-14 23:24:23 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,9 @@ def speak_to_grandma(grandma)
   if grandma == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   elsif grandma.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SONNY!"
   end
 end
 
